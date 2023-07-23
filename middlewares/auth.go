9da3ac39d49f2f7ecbadd03e0b3cc4cb938f8a37
Maria Papanagiotou <mariapapanagiwtou@gmail.com>
2023-07-23 19:00:01 +0300

    fix auth

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -5,12 +5,18 @@ import (
 	"github.com/golang-jwt/jwt"
 	jwtService "github.com/scarlettmiss/bestPal/application/services/jwtService"
 	"net/http"
+	"strings"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		const BEARER_SCHEMA = "Bearer "
 		authHeader := c.GetHeader("Authorization")
+		if !strings.Contains(authHeader, BEARER_SCHEMA) {
+			c.JSON(401, gin.H{"error": "request does not contain an access token"})
+			c.Abort()
+			return
+		}
 		tokenString := authHeader[len(BEARER_SCHEMA):]
 		if tokenString == "" {
 			c.JSON(401, gin.H{"error": "request does not contain an access token"})
